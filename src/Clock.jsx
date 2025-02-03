bf26d87bbba516a0f410b385999083f0dcf814d4
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-02-03 17:06:31 +0100

    feat: update clocks with useState

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,11 +1,25 @@
+
+import { use } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 function Clock({country, timeshow}) {
     // console.log(props) //le props passate dalle componenti si presentano come oggetti
     
     const t = Date.now() + 3600 * timeshow * 1000; //calcolo della data in base alla timezone
     
-    const date = new Date(t);
+    const dateIni = new Date(t);
     
-    console.log(date, t, timeshow);
+    //console.log(date, t, timeshow);
+
+    const [date, setDate] = useState(dateIni);
+
+
+    setTimeout(() => {
+        const t = date.getTime() + 3600 * 1000
+        setDate(new Date(t))
+    }, 5000);
+    
     return (
         <h3> 
             In {country} today is {date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}
